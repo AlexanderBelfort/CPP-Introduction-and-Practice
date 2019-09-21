61183e36cf7ad9eaaa1669ab6923dfbebc08c8ff
Alexander Belfort <36893759+AlexanderBelfort@users.noreply.github.com>
2019-09-21 16:31:57 +0100

    Add files via upload

---
+++ BMI.cpp
@@ -0,0 +1,30 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main() {
+
+	double height, weight, bmi;
+
+	//User must input height
+
+	std::cout << "Type in your height (m): ";
+	std::cin >> height;
+
+	//User must input weight
+
+	std::cout << "Type in your weight (kg): ";
+	std::cin >> weight;
+
+	//Our bmi formula
+
+	bmi = weight / (height*height);
+
+	std::cout << bmi;
+
+
+	return 0;
+
+}
